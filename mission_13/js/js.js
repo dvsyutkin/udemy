30a4019d0ff4bb27bc9791ecbe3099e4710babca
Denis <examples@mail.ru>
2020-01-25 23:11:08 +0300

    Задание 13. AJAX
    Отправка форм.

--- mission_13/js/js.js
+++ mission_13/js/js.js
@@ -40,7 +40,7 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 
 
-// кнопка на вверх
+    // кнопка на вверх
     let btnTop = document.querySelector('.btn-top');
     window.addEventListener('scroll', () => {
         let scrollTop = document.documentElement.scrollTop.toFixed();
@@ -55,7 +55,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
         let top = setInterval(() => {
             if (document.documentElement.scrollTop >= 10) {
-                document.documentElement.scrollTop -= 30; 
+                document.documentElement.scrollTop -= 30;
             } else {
                 clearInterval(top);
             }
@@ -69,24 +69,24 @@ window.addEventListener('DOMContentLoaded', function () {
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
-            seconds = Math.floor((t/1000) % 60),
-            minutes = Math.floor((t/1000/60) % 60),
-            hours = Math.floor(t/(1000 * 60 * 60));
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor(t / (1000 * 60 * 60));
 
-            return {
-                'total' : t,
-                'hours' : hours,
-                'minutes' : minutes,
-                'seconds' : seconds
-            };
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
     }
 
     function setClock(id, endtime) {
-        const   timer = document.getElementById(id),
-                hours = timer.querySelector('.hours'),
-                minutes = timer.querySelector('.minutes'),
-                seconds = timer.querySelector('.seconds'),
-                timerId = setInterval(updateClock, 1000);
+        const timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timerId = setInterval(updateClock, 1000);
 
         function updateClock() {
             let t = getTimeRemaining(endtime);
@@ -107,7 +107,7 @@ window.addEventListener('DOMContentLoaded', function () {
             hours.textContent = addZero(t.hours);
             minutes.textContent = addZero(t.minutes);
             seconds.textContent = addZero(t.seconds);
-            
+
             if (t.total <= 0) {
                 clearInterval(timerId);
                 hours.textContent = '00';
@@ -139,7 +139,7 @@ window.addEventListener('DOMContentLoaded', function () {
     let descriptionBtn = document.querySelectorAll('.description-btn');
 
     descriptionBtn.forEach(element => {
-        element.addEventListener('click', function() {
+        element.addEventListener('click', function () {
             overlay.style.display = 'block';
             this.classList.add('more-splash');
             document.body.style.overflow = 'hidden';
@@ -149,31 +149,37 @@ window.addEventListener('DOMContentLoaded', function () {
     // отправка формы
 
     let message = {
-        loading: 'Идет отправка сообщения...',
-        succes: 'Спасибо за ображения, мы свяжемся с Вами',
-        failure: 'Произошла какая-о ошибка'
-    },
+            loading: 'Идет отправка сообщения...',
+            succes: 'Спасибо за ображения, мы свяжемся с Вами',
+            failure: 'Произошла какая-о ошибка'
+        },
+        forms = document.querySelectorAll('form'),
         mainForm = document.querySelector('.main-form'),
-        input = mainForm.querySelectorAll('input'),
         sendMessage = document.createElement('div');
-        
-        sendMessage.classList.add('status');
 
-        mainForm.addEventListener('submit', (event) => {
+    sendMessage.classList.add('status');
+
+
+    forms.forEach(element => {
+        element.addEventListener('submit', function (event) {
+
+            let input = element.querySelectorAll('input');
             event.preventDefault();
-            mainForm.appendChild(sendMessage);
+
+            element.appendChild(sendMessage);
 
             let request = new XMLHttpRequest();
             request.open("POST", "server.php");
             //request.setRequestHeader('Content-type', 'application/x-www-form-urlencode'); // заголовок для FormData
             request.setRequestHeader('Content-type', 'application/json; charset=utf-8'); // заголовок для JSON
 
-            let formData = new FormData(mainForm);
+            let formData = new FormData(element);
 
             let obj = {}; // временный объект для конверта из FormData в JSON формат
             formData.forEach((value, key) => {
                 obj[key] = value;
             });
+
             let json = JSON.stringify(obj); // конвертация в JSON формат
             request.send(json);
 
@@ -183,15 +189,51 @@ window.addEventListener('DOMContentLoaded', function () {
                 } else if (request.readyState === 4 && request.status == 200) {
                     sendMessage.innerHTML = message.succes;
 
-                    input.forEach(element => {
-                        element.value = '';
+                    input.forEach(elementInput => {
+                        elementInput.value = '';
                     });
 
                 } else {
                     sendMessage.innerHTML = message.failure;
                 }
             });
-
         });
-    
+    });
+
+
+    // mainForm.addEventListener('submit', (event) => {
+    //     event.preventDefault();
+    //     mainForm.appendChild(sendMessage);
+
+    //     let request = new XMLHttpRequest();
+    //     request.open("POST", "server.php");
+    //     //request.setRequestHeader('Content-type', 'application/x-www-form-urlencode'); // заголовок для FormData
+    //     request.setRequestHeader('Content-type', 'application/json; charset=utf-8'); // заголовок для JSON
+
+    //     let formData = new FormData(mainForm);
+
+    //     let obj = {}; // временный объект для конверта из FormData в JSON формат
+    //     formData.forEach((value, key) => {
+    //         obj[key] = value;
+    //     });
+    //     let json = JSON.stringify(obj); // конвертация в JSON формат
+    //     request.send(json);
+
+    //     request.addEventListener('readystatechange', () => {
+    //         if (request.readyState < 4) {
+    //             sendMessage.innerHTML = message.loading;
+    //         } else if (request.readyState === 4 && request.status == 200) {
+    //             sendMessage.innerHTML = message.succes;
+
+    //             input.forEach(element => {
+    //                 element.value = '';
+    //             });
+
+    //         } else {
+    //             sendMessage.innerHTML = message.failure;
+    //         }
+    //     });
+
+    // });
+
 });
